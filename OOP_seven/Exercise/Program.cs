4e55b974a49efe61e5d4e89264a643b572540d96
Luiz <l.guilherme128@gmail.com>
2023-11-20 14:39:16 -0300

    .

--- OOP_seven/Exercise/Program.cs
+++ OOP_seven/Exercise/Program.cs
@@ -10,7 +10,9 @@ namespace Exercise
             Pension[] a = new Pension[p.n];
             for (int i = 0; i < p.n; i++)
             {
+                Console.WriteLine("Nome: ");
                 string nm = Console.ReadLine();
+                Console.WriteLine("Email: ");
                 string em = Console.ReadLine();
                 double r = 300;
                 if (i > 0)
