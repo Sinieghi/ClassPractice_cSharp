f960607c3d10545b1954e77b16029d7300d206a3
Luiz <l.guilherme128@gmail.com>
2023-11-23 20:00:52 -0300

    Praticando enumerações e composições

---
+++ OOP_thirteen/Entities/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new()
+            {
+                Id = 1116,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            string tx = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(tx);
+        }
+    }
+}
