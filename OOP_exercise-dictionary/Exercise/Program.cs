776c5deead202ed7f8384b01f1ea50c99fa01c30
Luiz <l.guilherme128@gmail.com>
2024-01-23 16:02:16 -0300

    Election result exercise

---
+++ OOP_exercise-dictionary/Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\DOC\c#OOP\file_bridge\in2.txt";
+            try
+            {
+                Dictionary<string, int> keys = [];
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    int k = -1;
+                    string sr1 = "";
+                    string sr2 = "";
+                    string? fStr = sr.ReadLine();
+                    for (int i = 0; i < fStr?.Length; i++)
+                    {
+                        if (fStr[i] == ',') { k = i; i++; }
+                        if (k == -1) { sr1 += fStr[i]; }
+                        else sr2 += fStr[i];
+                    }
+                    keys[sr1] = int.Parse(sr2);
+                }
+                System.Console.WriteLine("All search results");
+                foreach (KeyValuePair<string, int> k in keys)
+                {
+                    System.Console.WriteLine(k.Key + ": " + k.Value);
+                }
+                System.Console.WriteLine("total: " + keys.Count);
+            }
+            catch (IOException e)
+            {
+                System.Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+    }
+}
---
+++ OOP_thirtyseven-dictionary/Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = [];
+
+            cookies["user"] = "Luiz";
+            cookies["email"] = "L.guilherme@mail.com";
+            cookies["phone"] = "95949494949";
+            //rewrites above values
+            cookies["phone"] = "6545464665";
+
+            System.Console.WriteLine(cookies["user"]);
+            cookies.Remove("user");
+            if (cookies.ContainsKey("user"))
+            {
+                System.Console.WriteLine(cookies["user"]);
+            }
+            else System.Console.WriteLine("No such user.");
+
+            System.Console.WriteLine("Size: " + cookies.Count);
+
+            //Dictionary iterator
+            System.Console.WriteLine("all cookies");
+            //you can just use var item in cookies instead all this syntax
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                System.Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
